55d68f2a96abb5ea4ccb041ffd0830a8338d6a70
Kevin Van de Vyver <kevinvandevyver1@gmail.com>
2018-11-29 10:23:36 +0100

    added navigation

--- FrontEnd/Dashboard/src/app/app.module.ts
+++ FrontEnd/Dashboard/src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule} from 'angular-bootstrap-md';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CameraService } from './Services/camera.service';
+import { NavbarComponent } from './Navigatie/navbar.component';
+import { HomeComponent } from './Home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "page1", component: HomeComponent},
+      { path: "", redirectTo: "page1", pathMatch: "full" },
+    ], { useHash: false})
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [
