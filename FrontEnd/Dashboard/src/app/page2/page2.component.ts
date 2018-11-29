3431bbc1f9afd4f65570d1685c4eb71e40f33d1e
Unknown <inias--@hotmail.com>
2018-11-29 11:05:14 +0100

    foto's pakken van stream

--- FrontEnd/Dashboard/src/app/page2/page2.component.ts
+++ FrontEnd/Dashboard/src/app/page2/page2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-Home',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
 })
 
 export class Page2Component{
-    
+  
+    @ViewChild("video")
+    public video: ElementRef;
+
+    @ViewChild("canvas")
+    public canvas: ElementRef;
+
+    public captures: Array<any>;
+
+    public constructor() {
+        this.captures = [];
+    }
+
+    public ngOnInit() { }
+
+    /*public ngAfterViewInit() {
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                this.video.nativeElement.src = "http://172.21.1.122:8080/stream/camera-1";//window.URL.createObjectURL(stream);
+                this.video.nativeElement.play();
+            });
+        }
+    }*/
+
+
+capture(){
+    var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+}
+
+
+
 }
