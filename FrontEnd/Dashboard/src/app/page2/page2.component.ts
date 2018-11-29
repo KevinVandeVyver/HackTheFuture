8d950b9803146f14b8ee893f3eb5bce5ec3aa031
Unknown <inias--@hotmail.com>
2018-11-29 11:27:34 +0100

    css

--- FrontEnd/Dashboard/src/app/page2/page2.component.ts
+++ FrontEnd/Dashboard/src/app/page2/page2.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 @Component({
     selector: 'app-Home',
     templateUrl: './page2.component.html',
+    styleUrls: ['./page2.component.css']
 })
 
 export class Page2Component{
@@ -21,10 +22,10 @@ export class Page2Component{
 
     public ngOnInit() { }
 
-    /*public ngAfterViewInit() {
+ /*   public ngAfterViewInit() {
         if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-                this.video.nativeElement.src = "http://172.21.1.122:8080/stream/camera-1";//window.URL.createObjectURL(stream);
+                this.video.nativeElement.src = window.URL.createObjectURL(stream);
                 this.video.nativeElement.play();
             });
         }
@@ -33,7 +34,8 @@ export class Page2Component{
 
 capture(){
     var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
-    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    this.captures.push(this.canvas.nativeElement);//this.canvas.nativeElement.toDataURL("image/png"));
+    
 }
 
 
